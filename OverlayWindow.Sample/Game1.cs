a416ae537987a8898b118cbc8433f6f7a51667ac
Johnson <j3.soon@msa.hinet.net>
2018-02-27 17:57:34 +0800

    Add OverlayGame and Sample

---
+++ OverlayWindow.Sample/Game1.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace OverlayWindow.Sample
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class Game1 : OverlayGame
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        Texture2D icon;
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            graphics.PreferredBackBufferWidth = System.Windows.Forms.Screen.PrimaryScreen.Bounds.Width;
+            graphics.PreferredBackBufferHeight = System.Windows.Forms.Screen.PrimaryScreen.Bounds.Height;
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+            icon = Content.Load<Texture2D>("Icon");
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// game-specific content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(new Color(0, 0, 0, 0));
+
+            // TODO: Add your drawing code here
+            spriteBatch.Begin();
+            Vector2 center = new Vector2(Window.ClientBounds.Width / 2f, Window.ClientBounds.Height / 2f);
+            Vector2 origin = new Vector2(icon.Width / 2f, icon.Height / 2f);
+            spriteBatch.Draw(icon, center, null, new Color(Color.White, 128), 0, origin, 1, SpriteEffects.None, 0);
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ OverlayWindow/OverlayGame.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.InteropServices;
+using Microsoft.Xna.Framework;
+
+namespace OverlayWindow
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class OverlayGame : Game
+    {
+        #region DllImports
+        [DllImport("kernel32.dll")]
+        static extern void SetLastError(uint dwErrCode);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern bool SetLayeredWindowAttributes(IntPtr hwnd, uint crKey, byte bAlpha, uint dwFlags);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int X, int Y, int cx, int cy, uint uFlags);
+
+        [DllImport("dwmapi.dll")]
+        static extern int DwmExtendFrameIntoClientArea(IntPtr hWnd, ref int[] pMarInset);
+
+        const int GWL_EXSTYLE = -20;
+        const int LWA_ALPHA = 0x00000002;
+        const int WS_EX_LAYERED = 0x00080000;
+        const int WS_EX_TRANSPARENT = 0x00000020;
+        const int WS_EX_NOACTIVATE = 0x08000000;
+        IntPtr HWND_TOPMOST = (IntPtr)(-1);
+        const int SWP_NOMOVE = 0x0002;
+        const int SWP_NOSIZE = 0x0001;
+        const int S_OK = 0x00000000;
+        #endregion
+
+        public OverlayGame()
+        {
+            Window.IsBorderless = true;
+
+            // Set to layered, transparent window.
+            SetLastError(0);
+            int ret = SetWindowLong(Window.Handle, GWL_EXSTYLE, WS_EX_LAYERED | WS_EX_TRANSPARENT | WS_EX_NOACTIVATE);
+            if (ret == 0 && Marshal.GetLastWin32Error() != 0)
+                throw new Win32Exception(Marshal.GetLastWin32Error());
+
+            // Set to top-most window.
+            if (!SetWindowPos(Window.Handle, HWND_TOPMOST, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE))
+                throw new Win32Exception(Marshal.GetLastWin32Error());
+
+            // Required in order to make layered window visible.
+            if (!SetLayeredWindowAttributes(Window.Handle, 0, 255, LWA_ALPHA))
+                throw new Win32Exception(Marshal.GetLastWin32Error());
+
+            int[] margins = { -1 };
+            if ((ret = DwmExtendFrameIntoClientArea(Window.Handle, ref margins)) != S_OK)
+                throw new Win32Exception(ret);
+        }
+    }
+}
